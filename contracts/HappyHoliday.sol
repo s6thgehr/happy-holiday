5231d74122ffbd730df0a1adaac3ca2e11e673aa
Thomas Gehrmann <s6thgehr@gmail.com>
2022-05-11 17:13:23 +0200

    Create function to retrieve rain data

--- contracts/HappyHoliday.sol
+++ contracts/HappyHoliday.sol
@@ -2,10 +2,53 @@
 
 pragma solidity 0.8.7;
 
-contract HappyHoliday {
+import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
+
+contract HappyHoliday is ChainlinkClient {
+    using Chainlink for Chainlink.Request;
+
+    bytes32 public totalRainJobId;
+    uint256 public totalRain;
+    uint256 public fee;
+
+    event TotalRain(uint256 _result);
+
     uint256 public constant DECIMALS = 10**18;
     uint256 public insuredValue;
     uint256 public premium;
+
+    constructor(
+        address _link,
+        address _oracle,
+        bytes32 _totalRainJobId,
+        uint256 _fee
+    ) {
+        setChainlinkToken(_link);
+        setChainlinkOracle(_oracle);
+        totalRainJobId = _totalRainJobId;
+        fee = _fee;
+    }
+
+    function requestTotalRain(string memory _from, string memory _to) external {
+        Chainlink.Request memory req = buildChainlinkRequest(
+            totalRainJobId,
+            address(this),
+            this.fulfillTotalRain.selector
+        );
+        req.add("dateFrom", _from);
+        req.add("dateTo", _to);
+        req.add("method", "SUM");
+        req.add("column", "prcp");
+        sendChainlinkRequest(req, fee);
+    }
+
+    function fulfillTotalRain(bytes32 _requestId, uint256 _result)
+        external
+        recordChainlinkFulfillment(_requestId)
+    {
+        totalRain = _result;
+        emit TotalRain(_result);
+    }
 
     function setInsuredValue(uint256 value) external {
         insuredValue = value * DECIMALS;
