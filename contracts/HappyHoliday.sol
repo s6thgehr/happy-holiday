e6cbd7d02397c2283ba5bf6709c5a3fd76ae3a09
Thomas Gehrmann <s6thgehr@gmail.com>
2022-05-16 18:29:20 +0200

    Add helper functions

--- contracts/HappyHoliday.sol
+++ contracts/HappyHoliday.sol
@@ -16,12 +16,15 @@ contract HappyHoliday is
     bytes32 public rainJobId;
     bytes32 public locationJobId;
     uint256 public fee;
+    int256 constant rainMultiplier = 1000;
 
     uint256 public rainPast24h;
     uint256 public locationKey;
 
     uint256 public constant interval = 86400; // One day intervall
     uint256 public lastTimeStamp;
+
+    uint256 public policyId;
 
     enum PolicyStatus {
         CREATED, // Policy is subscribed
@@ -31,11 +34,30 @@ contract HappyHoliday is
         PAIDOUT // Claim is paid out
     }
 
+    struct Policy {
+        uint256 id;
+        address payable beneficiary;
+        uint256 locationKey;
+        uint256 startDate;
+        uint256 endDate;
+        uint256 insuredValue;
+        uint256 premium;
+        uint256 rainTreshold;
+        uint8 incidentsTreshold;
+        uint8 incidents;
+    }
+
+    mapping(uint256 => address) policyIdToBuyer;
+    mapping(address => uint256) buyerPolicyCount; // Number of policies someone bought
+
+    Policy[] public policies;
+
     event RequestRainFulfilled(bytes32 indexed requestId, uint256 indexed rain);
     event RequestLocationFulfilled(
         bytes32 indexed requestId,
         uint256 indexed key
     );
+    event PolicySubscription(address indexed beneficiary, uint256 indexed id);
 
     constructor(
         address _link,
@@ -50,6 +72,65 @@ contract HappyHoliday is
         locationJobId = _locationJobId;
         fee = _fee;
         lastTimeStamp = block.timestamp;
+    }
+
+    function balanceOf(address _buyer) public view returns (uint256 _balance) {
+        return buyerPolicyCount[_buyer];
+    }
+
+    function buyerOf(uint256 _policyId) public view returns (address _buyer) {
+        return policyIdToBuyer[_policyId];
+    }
+
+    function getPoliciesByBuyer(address _buyer)
+        external
+        view
+        returns (Policy[] memory)
+    {
+        Policy[] memory result = new Policy[](buyerPolicyCount[_buyer]);
+        uint256 counter = 0;
+        for (uint256 i = 0; i < policies.length; i++) {
+            if (policyIdToBuyer[policies[i].id] == _buyer) {
+                result[counter] = policies[i];
+                counter++;
+            }
+        }
+        return result;
+    }
+
+    function subscribePolicy(
+        uint256 _startDate,
+        uint256 _endDate,
+        uint256 _insuredValue,
+        uint256 _premium,
+        uint8 _incidentsTreshold
+    ) public payable {
+        require(_premium == msg.value, "You have to pay the exact Premium");
+
+        uint256 _rainThreshold = uint256(50 * rainMultiplier); // 50 mm rain
+
+        // Get locationKey: _locationKey
+        uint256 _locationKey = 0;
+
+        Policy memory policy = Policy({
+            id: policyId,
+            beneficiary: payable(msg.sender),
+            locationKey: _locationKey,
+            startDate: _startDate,
+            endDate: _endDate,
+            insuredValue: _insuredValue,
+            premium: _premium,
+            rainTreshold: _rainThreshold,
+            incidentsTreshold: _incidentsTreshold,
+            incidents: 0
+        });
+
+        policyIdToBuyer[policyId] = msg.sender;
+        buyerPolicyCount[msg.sender]++;
+        policies.push(policy);
+
+        emit PolicySubscription(msg.sender, policyId);
+        policyId++;
     }
 
     /**********  REQUEST ORACLE DATA **********/
@@ -81,7 +162,7 @@ contract HappyHoliday is
         );
         req.add("get", url);
         req.add("path", "0,PrecipitationSummary,Past24Hours,Metric,Value");
-        req.addInt("times", 1000);
+        req.addInt("times", rainMultiplier);
         sendChainlinkRequest(req, fee);
     }
 
